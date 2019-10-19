15909fd4e4e2eed8c650cc5917a86f3242617d86
smozume <a-hizume@smolt-stage.com>
2019-10-20 01:51:03 +0900

    bat追加

---
+++ csv3_extra92.bat
@@ -0,0 +1,214 @@
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20060101 20100101 20130101 true
+rem  python manage.py train_csv_create92 train train_dir1509_2010 1509   20050301 20100101 20100101 true
+rem     python manage.py train_csv_create92 train train_dir1509_2010 1509   20040101 20100101 20100101 true
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20010101 20100101 20100101 true
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20040401 20100101 20100101 true
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20050101 20100101 20100101 true
+rem    python manage.py train_csv_create92 train train_dir1509_2010 1509   20010101 20120101 20120101 true
+rem    python manage.py train_csv_create92 train train_dir1509_99 1509 20111231 20120101 20191231 true
+rem   python manage.py train_csv_create92 train train_dir1509_2010 1509   20010101 20100101 20100101 true
+rem      python manage.py train_csv_create92 train train_dir1509_99 1509 20091231 20100101 20191231 true
+rem  python manage.py train_csv_create92 train train_dir1509_2010 1509   20114101 20170101 20170101 true
+rem  python manage.py train_csv_create92 train train_dir1509_99 1509 20161231 201700101 20191231 true
+rem  python manage.py train_csv_create92 train train_dir1509_2010 1509   20070101 20120101 20120101 true
+rem  python manage.py train_csv_create92 train train_dir1509_99 1509 20111231 201200101 20191231 true
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20020101 20140101 20140101 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20131231 201400101 20191231 true
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20010101 20080101 20080101 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20071231 20080101 20191231 true
+
+rem  python manage.py train_csv_create92 train train_dir1509_2010 1509   20010101 20100101 20100101 true
+rem  python manage.py train_csv_create92 train train_dir1509_99 1509 20121231 20130101 20191231 true
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20040101 20160101 20160101 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20151231 20160101 20191231 true
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20040601 20100101 20100101 true
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20031101 20100101 20100101 true
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20050301 20100101 20100101 true
+rem  python manage.py train_csv_create92 train train_dir1509_2010 1509   20040401 20100101 20100101 true
+rem  python manage.py train_csv_create92 train train_dir1509_99 1509 20091231 20100101 20191231 true
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20050101 20140101 20140101 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20131231 20140101 20191231 true
+
+rem  python manage.py train_csv_create92 train train_dir1509_2010 1509   20050115 20100101 20100101 true
+  python manage.py train_csv_create92 train train_dir1509_2010 1509   20050301 20100101 20100101 true
+  python manage.py train_csv_create92 train train_dir1509_99 1509 20091231 20100101 20191231 true
+
+rem  python manage.py train_csv_create92 train train_dir1509_2010 1509   20050115 20100201 20100201 true
+rem  python manage.py train_csv_create92 train train_dir1509_99 1509 20100131 20100201 20191231 true
+
+
+rem					python manage.py train_csv_create92 train train_dir1509_2010 1509   20070101 20120101 20120101 true
+rem					python manage.py train_csv_create92 train train_dir1509_99 1509 20111231 20120101 20191231 true
+
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20020101 20150901 20150901 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20141231 20150901 20191231 true
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20020101 20130101 20130101 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20121231 20130101 20191231 true
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20120101 20170101 20170101 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20161231 20170101 20191231 true
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20130101 20190101 20190101 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20171231 20190101 20191231 true
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20030101 20140101 20140101 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20131231 20140101 20191231 true
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20000101 20140101 20140101 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20131231 20140101 20191231 true
+
+rem  python manage.py train_csv_create92 train train_dir1509_2010 1509   20040401 20090101 20090101 true
+rem  python manage.py train_csv_create92 train train_dir1509_99 1509 20081231 20090101 20191231 true
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20030101 20130101 20130101 true
+           rem python manage.py train_csv_create92 train train_dir1509_99 1509 20121231 20130101 20191231 true
+
+rem  python manage.py train_csv_create92 train train_dir1509_2010 1509   20010101 20100101 20100101 true
+rem  python manage.py train_csv_create92 train train_dir1509_99 1509 20091231 20100101 20191231 true
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20050301 20100101 20100101 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20091231 20100101 20191231 true
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20050301 20150901 20150901 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20141231 20150901 20191231 true
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20090301 20140101 20142101 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20131231 20140101 20191231 true
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20100101 20170101 20170101 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20161231 20170101 20191231 true
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20040401 20100101 20100101 true
+rem python manage.py train_csv_create92 train train_dir1509_99 1509 20091231 20100101 20191231 true
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20040101 20180101 20180101 true
+
+rem python manage.py train_csv_create92 train train_dir1509_2010 1509   20100101 20190901 20190901 true
+rem  python manage.py train_csv_create92 train train_dir1509_99 1509 20041231 20050101 20091231 true
+
+
+rem   python manage.py train_csv_create92 train train_dir1509_2010 1509   20140301 20190101 20190101 true
+rem   python manage.py train_csv_create92 train train_dir1509_99 1509 20181231 20190101 20191231 true
+rem              python manage.py train_csv_create92 train train_dir1509_99 1509   20071231 20080101 20140301 true
+
+rem              python manage.py train_csv_create92 train train_dir1509_2010 1509   20080301 20170101 20170101 true
+rem              python manage.py train_csv_create92 train train_dir1509_99 1509 20161231 20170101 20191231 true
+rem              python manage.py train_csv_create92 train train_dir1509_99 1509   20071231 20080101 20130301 true
+rem              python manage.py train_csv_create92 train train_dir1509_2010 1509   20160101 20190515 20190515 true
+rem              python manage.py train_csv_create92 train train_dir1509_99 1509 20190531 20190601 20191231 true
+rem              python manage.py train_csv_create92 train train_dir1509_2010 1509   20140101 20190515 20190515 true
+rem              python manage.py train_csv_create92 train train_dir1509_99 1509 20190531 20190601 20191231 true
+
+
+rem              python manage.py train_csv_create92 train train_dir1509_2010 1509   20100301 20160101 20160101 true
+rem              python manage.py train_csv_create92 train train_dir1509_99 1509 20151231 20160101 20191231 true
+
+
+rem  python manage.py train_csv_create92 train train_dir1509_2010 1509   20090101 20140101 20140101 true
+rem  python manage.py train_csv_create92 train train_dir1509_99 1509 20131231 20140101 20191231 true
+
+
+   python manage.py csv_conv77_extratrees train_dir1509_2010
+   python manage.py csv_conv77_extratrees train_dir1509_99
+
+rem  python manage.py csv_conv77_extratrees_Data_Augmentation train_dir1509_2010
+rem  python manage.py csv_conv77_extratrees train_dir1509_99
+
+rem python manage.py csv_conv77_extratrees_top train_dir1509_2010
+rem python manage.py csv_conv77_extratrees_top train_dir1509_99
+
+rem  python manage.py csv_conv77_extratrees_chakusa train_dir1509_2010
+rem  python manage.py csv_conv77_extratrees_chakusa train_dir1509_99
+
+rem    python manage.py csv_conv77_extratrees_single_race train_dir1509_2010
+rem    python manage.py csv_conv77_extratrees_single_race train_dir1509_99
+
+rem python manage.py csv_conv77_extratrees_henkan train_dir1509_2010
+rem python manage.py csv_conv77_extratrees_henkan train_dir1509_99
+
+
+
+@echo off
+
+copy  d:\wk\train_dir1509_99\*test_class.csv d:\wk\train_dir1509_2010
+ python manage.py train_forest_exec77_extratrees train_dir1509_2010   xgboost735_35
+
+
+
+
+
+REM 遅延環境変数(!i!)の有効化
+setlocal ENABLEDELAYEDEXPANSION
+
+REM 添え字をつけて変数を定義
+set VAR_1= xgboost735_35
+
+
+rem  set VAR_2=keras001
+rem  set VAR_3=KMxg001
+rem  set VAR_4=xgboost735_60
+rem  set VAR_5=lgb001
+rem  set VAR_6=extratrees809
+rem  set VAR_7=extratrees810
+rem  set VAR_8=xgboost735_35
+rem  set VAR_9=xgboost793
+rem  set VAR_10=xgboost793_9
+rem  set VAR_11=xgboost793_10
+rem  set VAR_12=xgboost735_68
+rem  set VAR_13=extratrees816
+rem  set VAR_14=extratrees817
+rem  set VAR_15=extratrees818
+rem  set VAR_16=extratrees819
+rem  set VAR_17=extratrees820
+rem  set VAR_18=extratrees741
+
+ set DT_1=
+ set DT_2=
+ set DT_3=
+ set DT_4=
+ set DT_5=
+ set DT_6=
+ set DT_7=
+ set DT_8=
+ set DT_9=
+ set DT_10=
+ set DT_11=
+ set DT_12=
+ set DT_13=
+ set DT_14=
+ set DT_15=
+ set DT_16=
+ set DT_17=
+ set DT_18=
+
+REM foreachループ処理
+set i=1
+:BEGIN
+call set it=%%VAR_!i!%%
+call set dt=%%DT_!i!%%
+if defined it (
+rem	python manage.py train_csv_create92 train train_dir1509_2010 1509   !dt! 20150901 20150901 true
+rem	python manage.py csv_conv77_extratrees train_dir1509_2010
+rem	python manage.py train_forest_exec77_extratrees train_dir1509_2010 !it!
+
+rem	 del d:\wk\train_dir1509_2010\*test_class.csv
+	 copy  d:\wk\train_dir1509_99\*test_class.csv d:\wk\train_dir1509_2010
+rem	 copy  d:\wk\train_dir1053_99\*test_class.csv d:\wk\train_dir1053_2010
+rem	 python d:\root\workspace\jra\manage.py test_forest_fillter_create_small_extratrees_gousei train_dir1509_2010 !it! true
+ python d:\root\workspace\jra\manage.py test_forest_fillter_create_small_extratrees train_dir1509_2010 !it! true
+rem  python d:\root\workspace\jra\manage.py test_forest_fillter_create_small_extratrees_regressor train_dir1509_2010 !it! true
+
+
+rem	python d:\root\workspace\jra\manage.py insert_fillter_result train_dir1509_2010  !it! !i!s
+
+	  set /A i+=1
+	  goto :BEGIN
+)
+
+
+
+
+
